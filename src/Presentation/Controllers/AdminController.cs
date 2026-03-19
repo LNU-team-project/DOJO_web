d70a51af8c55c05750307b79a93dba64f7535146
Solomiia Chvartkovska <cvartsola@gmail.com>
2026-03-19 23:12:48 +0200

    Admin Visual

---
+++ src/Presentation/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DOJO2.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Presentation/ViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DOJO2.Presentation.ViewModels
+{
+    public class AdminLoginViewModel
+    {
+        [Required(ErrorMessage = "Ім'я користувача є обов'язковим")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Пароль є обов'язковим")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
